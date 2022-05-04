bb5f1ce5c9ce02dbe4ee14419bc4d14d2b290754
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-05-04 17:32:30 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],c=0,k;     scanf("%d%d",&n,&k);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%k!=0)         {             c++;         }     }     printf("%d",c); }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],c=0,k;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%k!=0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
