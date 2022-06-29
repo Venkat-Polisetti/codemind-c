11e3a0bebfe6e384c99d3a5b7ffd12ce3f623fa8
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-06-29 22:10:10 +0530

    #include<stdio.h> int main() {     int n,d,rev=0,i,c=0,j,k=0,temp=0;     scanf("%d",&n);     temp=n;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }    while(c==2&&n>0)    {        d=n%10;        rev=rev*10+d;        n=n/10;    }    for(j=1;j<=rev;j++)    {        if(rev%j==0)        {            k++;       }    }    if(k==2&&c==2)    {        printf("circular prime");    }    else if(c==2&&k>2)    {        printf("prime but not a circular prime");    }    else    {        printf("not prime");    } }

--- circular_prime.c
+++ circular_prime.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,d,rev=0,i,j,a=0,temp=0,c=0;
+    int n,d,rev=0,i,c=0,j,k=0,temp=0;
     scanf("%d",&n);
     temp=n;
     for(i=1;i<=n;i++)
@@ -11,29 +11,28 @@ int main()
             c++;
         }
     }
-    while(c==2 && n>0)
-    {
-        d=n%10;
-        rev=rev*10+d;
-        n=n/10;
-    }
-    for(j=1;j<=rev;j++)
-    {
-        if(rev%j==0)
-        {
-           a++; 
-        }
-    }
-    if(a==2 && c==2)
-    {
-        printf("circular prime");
-    }
-    else if(c==2 && a>2)
-    {
-        printf("prime but not a circular prime");
-    }
-    else
-    {
-        printf("not prime");
-    }
+   while(c==2&&n>0)
+   {
+       d=n%10;
+       rev=rev*10+d;
+       n=n/10;
+   }
+   for(j=1;j<=rev;j++)
+   {
+       if(rev%j==0)
+       {
+           k++;       }
+   }
+   if(k==2&&c==2)
+   {
+       printf("circular prime");
+   }
+   else if(c==2&&k>2)
+   {
+       printf("prime but not a circular prime");
+   }
+   else 
+   {
+       printf("not prime");
+   }
 }
