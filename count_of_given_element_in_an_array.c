4d52d745d40892e24bc806de67eb2021cc8a2c10
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-06-30 04:55:14 +0530

    #include<stdio.h> int main() {     int n,arr[100],x,i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&x);     for(i=0;i<n;i++)     {         if(x==arr[i])         {             c++;         }     }     printf("%d",c); }

--- count_of_given_element_in_an_array.c
+++ count_of_given_element_in_an_array.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,arr[100],z,c=0;
+    int n,arr[100],x,i,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    scanf("%d",&z);
+    scanf("%d",&x);
     for(i=0;i<n;i++)
     {
-        if(z==arr[i])
+        if(x==arr[i])
         {
             c++;
         }
