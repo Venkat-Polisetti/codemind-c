499cc2f202d5b05cf033a2e8c348b08a686bd5f7
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-06-29 22:25:49 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     for(i=2;i<=n;i++)     {         if((i*i)==n)         {             c++;             printf("True");             break;         }     }     if(c==0)     {         printf("False");     } }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,20 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,x=0;
+    int n,i,c=0;
     scanf("%d",&n);
-    for(i=1;i<n;i++)
+    for(i=2;i<=n;i++)
     {
         if((i*i)==n)
         {
-            x=1;
+            c++;
+            printf("True");
+            break;
         }
     }
-    if(x==1)
-    {
-        printf("True");
-    }
-    else
+    if(c==0)
     {
         printf("False");
     }
