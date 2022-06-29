eefac40303d904bcaca8010d3fd01ac4b189e8f1
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-06-29 22:19:38 +0530

    #include<stdio.h> int main() {     int n,a[100],b[100],i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d%d",&a[i],&b[i]);     }     for(i=0;i<n;i++)     {         printf("%d ",a[i]+b[i]);     } }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,23 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int a[100][100],n,sum=0,i,j;
+    int n,a[100],b[100],i;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        for(j=0;j<2;j++)
-        {
-            scanf("%d",&a[i][j]);
-        }
+        scanf("%d%d",&a[i],&b[i]);
     }
     for(i=0;i<n;i++)
     {
-        sum=0;
-        for(j=0;j<2;j++)
-        {
-            sum+=a[i][j];
-        }
         printf("%d
-",sum);
+",a[i]+b[i]);
     }
 }
