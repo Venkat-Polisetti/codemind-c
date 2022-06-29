c820aee5561ca1fb1d99b9990a82ff312588b2df
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-06-30 04:56:29 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,min;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     min=arr[1];     for(i=0;i<n;i++)     {         if(min>arr[i])         {             min=arr[i];         }     }     printf("%d",min); }

--- minimum_element_of_an_array.c
+++ minimum_element_of_an_array.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,arr[100],min;
+    int n,arr[100],i,min;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    min=arr[0];
+    min=arr[1];
     for(i=0;i<n;i++)
     {
-       if(min>arr[i]) 
-       {
-           min=arr[i];
-       }
+        if(min>arr[i])
+        {
+            min=arr[i];
+        }
     }
     printf("%d",min);
 }
