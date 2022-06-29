866f168f3aeb725c76be77c866d2bbd0480924ff
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-06-29 22:21:18 +0530

    #include<stdio.h> int main() {     int n,d,x=0;     scanf("%d",&n);     while(n!=1)     {         if(n%2==0)         {             n=n/2;         }         else if(n%3==0)         {             n/=3;         }         else if(n%5==0)         {             n/=5;         }         else         {           printf("Not Ugly Number");           x=1;           break;         }     }     if(x==0)     {         printf("Ugly Number");     } }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,x=0;
+    int n,d,x=0;
     scanf("%d",&n);
     while(n!=1)
     {
         if(n%2==0)
         {
-            n/=2;
+            n=n/2;
         }
         else if(n%3==0)
         {
@@ -19,9 +19,9 @@ int main()
         }
         else
         {
-            printf("Not Ugly Number");
-            x=1;
-            break;
+          printf("Not Ugly Number");
+          x=1;
+          break;
         }
     }
     if(x==0)
