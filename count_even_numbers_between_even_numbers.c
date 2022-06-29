8e94d63636d2c97caec1c5296161579f442353f4
Venkat-Polisetti <97424586+Venkat-Polisetti@users.noreply.github.com>
2022-06-30 04:49:16 +0530

    #include<stdio.h> int main() {     int n,a[100],i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n-2;i++)     {         if(a[i+2]%2==0 && a[i]%2==0)         {             if(a[i+1]%2==0)             {                 c++;             }         }     }     printf("%d",c); }

--- count_even_numbers_between_even_numbers.c
+++ count_even_numbers_between_even_numbers.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,arr[100],c=0;
+    int n,a[100],i,c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
     for(i=0;i<n-2;i++)
     {
-        if(arr[i+2]%2==0 && arr[i]%2==0)
+        if(a[i+2]%2==0 && a[i]%2==0)
         {
-            if(arr[i+1]%2==0)
+            if(a[i+1]%2==0)
             {
                 c++;
             }
